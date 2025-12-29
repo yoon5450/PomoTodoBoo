52c75e9f4a75b284b41dcec7c9a7533bb9776e63
yoon5450 <dbseodnd356@naver.com>
2025-12-29 20:30:27 +0900

    Feat: TodoList 초기

--- PomoTodoBoo/src/components/home/TodoItem.vue
+++ PomoTodoBoo/src/components/home/TodoItem.vue
@@ -1,13 +1,49 @@
 <script setup lang="ts">
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+const data = defineProps<{ todo: Todo }>();
+
+const emit = defineEmits(['deleteTodo', 'updateTodo']);
+
+const deleteTodo = (id: number) => {
+  emit('deleteTodo', id);
+};
 
 </script>
 
 <template>
-  <div class="">
-    
-  </div>
+  <li class="todo-item">
+    <input type="checkbox" :checked="data.todo.completed" @change="emit('updateTodo', data.todo.id)" />
+    <span :class="{ 'line-through': data.todo.completed }">
+      {{ data.todo.title }}
+    </span>
+    <button @click="deleteTodo(data.todo.id)" class="deleteBtn">Del</button>
+  </li>
 </template>
 
 <style scoped>
+.line-through {
+  text-decoration: line-through;
+}
 
+.todo-item {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 1rem;
+  background-color: rgb(243, 243, 243);
+  padding: 0.5rem 1rem;
+}
+
+.deleteBtn {
+  background-color: #111111;
+  color: #ffffff;
+  border: none;
+  padding: 0.5rem;
+  cursor: pointer;
+}
 </style>
--- PomoTodoBoo/src/components/home/TodoList.vue
+++ PomoTodoBoo/src/components/home/TodoList.vue
@@ -1,7 +1,45 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue';
+import TodoItem from './TodoItem.vue';
+
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+const todos = ref<Todo[]>([
+  { id: 1, title: 'Todo 1', completed: false },
+  { id: 2, title: 'Todo 2', completed: false },
+]);
+
+const deleteTodo = (id: number) => {
+  todos.value = todos.value.filter((todo) => todo.id !== id);
+};
+
+const completeTodo = (id: number) => {
+  todos.value = todos.value.map((todo) => todo.id === id ? { ...todo, completed: !todo.completed } : todo)
+}
+
+</script>
 
 <template>
-  <div class=""></div>
+  <div class="todo-list-container">
+    <ul class="todo-list">
+      <TodoItem v-for="todo in todos" :key="todo.id" :todo="todo" @deleteTodo="deleteTodo" @updateTodo="completeTodo" />
+    </ul>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.todo-list-container {
+  width: 100%;
+}
+
+.todo-list {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem
+}
+</style>
